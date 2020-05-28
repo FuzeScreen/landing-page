a288c645b3caaf99723d22941a5c55173a416493
Felix Beer <developer@felix.beer>
2020-05-28 16:48:42 +0200

    -init commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'fuze-dark': '#202020',
+        'fuze-semidark': '#292929',
+        'fuze-orange': '#F39435',
+        'fuze-light': '#2e2e2e'
+      },
+      height: {
+        '96': '24rem',
+        '128': '32rem',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
